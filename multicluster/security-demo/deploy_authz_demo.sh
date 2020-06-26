38a3e587fe9a58023786e053cdca4c08a4c77158
Lei Tang <32078630+lei-tang@users.noreply.github.com>
2020-06-26 12:49:50 +0800

    Temporarily disable download Istio pkg to save some time for demo

--- multicluster/security-demo/deploy_authz_demo.sh
+++ multicluster/security-demo/deploy_authz_demo.sh
@@ -44,8 +44,8 @@ pwd
 
 # Download package that contains example service deployment files.
 # If you are testing a different ASM release, please replace the ASM release URL accordingly.
-gsutil cp gs://asm-staging-images/asm/istio-1.6.4-asm.8-linux-amd64.tar.gz .
-tar xzf istio-1.6.4-asm.8-linux-amd64.tar.gz
+#gsutil cp gs://asm-staging-images/asm/istio-1.6.4-asm.8-linux-amd64.tar.gz .
+#tar xzf istio-1.6.4-asm.8-linux-amd64.tar.gz
 export ISTIO=$(pwd)/istio-1.6.4-asm.8
 
 if [[ -z "${PROJECT_ID}" || -z "${CLUSTER_1}" || -z "${CLUSTER_2}" || -z "${LOCATION_1}" || -z "${LOCATION_2}" ]]; then
